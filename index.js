31abd1b74a989bf2ee930c6aea5b5a763e804fe1
AndyOlsen1295 <112365233+AndyOlsen1295@users.noreply.github.com>
2023-04-16 03:45:35 -0600

    final commit

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 //required files and programs.
 const inquirer = require("inquirer");
-// const generateHtml = require("./html.js")
+const generateHTML = require("./utils/generateHTML.js");
 const fs = require("fs");
 const Engineer = require("./lib/Engineer");
-// const generateManager = require("./employees/generateManager")
-// const generateDeveloper = require("./empolyees/generateDeveloper")
+
 const team = []
 
 // these questions will be how we pull in info on the Manager. 
@@ -31,7 +30,7 @@ const manager = [
   },
 ];
 
-// these through manager constructor. turn into object in constructor, then move to team array.
+// send these through manager constructor. turn into object in constructor, then move to team array.
 
 
 
@@ -58,14 +57,6 @@ const intern = [
     name: 'school',
     message: 'What school did they attend?',
   },
-  
-  //   if (list == 'y') {
-  //   //start back at the beginning of employees questions list!
-  //   return(employees);
-  // } else {
-  //   //be done
-  //   close
-  // }
 ];
 
 const engineer = [ // change employee to engineer.
@@ -89,7 +80,7 @@ const engineer = [ // change employee to engineer.
     name: 'github',
     message: 'What is their Github?', // Github
   },
-]
+];
 
 
 // function to write HTML
@@ -109,8 +100,8 @@ function initManager() {
 function initIntern() {
   inquirer.prompt (intern) .then ((answers=>{
     console.log(answers)
-    // run answers through manager constructor
-    // push new manager to global team array
+    // run answers through intern constructor
+    // push new intern to global team array
     menu()
   }))
 };
@@ -120,30 +111,36 @@ function initEngineer() {
     console.log(answers)
     // run answers through engineer constructor
     const emp = new Engineer(answers.name, answers.id, answers.email, answers.github)
-    // push new manager to global team array
+    // push new engineer to global team array
     team.push(emp)
     menu()
   }))
 };
 
 const menu = () => {
-  inquirer.prompt ([
+  inquirer.prompt([
     {
       type: 'list',
       name: 'choice',
-      message:'what type of empolyee do you want to create?',
-      choices:['intern', 'engineer','create team']
+      message: 'what type of empolyee do you want to create?',
+      choices: ['intern', 'engineer', 'create team']
     }
-  ]).then (answers => {
+  ]).then(answers => {
     if (answers.choice === 'intern') {
-      initIntern()
+      initIntern();
     } else if (answers.choice === 'engineer') {
-      initEngineer()
+      initEngineer();
     } else {
-      //run global team array through the HTML function.
-      // save the output of the html function to a file.
+      // generate HTML for team array
+      const html = generateHTML(team);
+
+      // write HTML to file
+      writeToFile('team.html', html);
+
+      console.log('Team HTML file written successfully!');
     }
-  }) 
-}
+  });
+};
+
 
 initManager();
--- utils/generateHTML.js
+++ utils/generateHTML.js
@@ -1,3 +1,54 @@
-function generateHTML(data)
+function generateHTML(team) {
+  let cards = "";
+  for (let i = 0; i < team.length; i++) {
+    let employee = team[i];
+    let specificProperty = "";
+    let specificValue = "";
+    if (employee.getRole() === "Manager") {
+      specificProperty = "Office Number";
+      specificValue = employee.getOffice();
+    } else if (employee.getRole() === "Engineer") {
+      specificProperty = "GitHub";
+      specificValue = employee.getGithub();
+    } else if (employee.getRole() === "Intern") {
+      specificProperty = "School";
+      specificValue = employee.getSchool();
+    }
+    cards += `
+    <div class="card">
+      <div class="card-header">
+        <h2>${employee.getName()}</h2>
+        <h3>${employee.getRole()}</h3>
+      </div>
+      <div class="card-body">
+        <ul>
+          <li>ID: ${employee.getId()}</li>
+          <li>Email: <a href="mailto:${employee.getEmail()}">${employee.getEmail()}</a></li>
+          <li>${specificProperty}: ${specificValue}</li>
+        </ul>
+      </div>
+    </div>
+    `;
+  }
+  let html = `
+  <!DOCTYPE html>
+  <html>
+    <head>
+      <meta charset="UTF-8">
+      <title>My Team</title>
+      <link rel="stylesheet" href="./style.css">
+    </head>
+    <body>
+      <header>
+        <h1>My Team</h1>
+      </header>
+      <main>
+        ${cards}
+      </main>
+    </body>
+  </html>
+  `;
+  return html;
+}
 
-// this is where the data.(answers) will go.
+module.exports = generateHTML;
